99b0655314c0cb8581ecff034bfbcde4ab7b91bb
arielcoral <123407136+arielcoral@users.noreply.github.com>
2023-05-31 18:06:51 +0300

    homework

---
+++ main.js
@@ -0,0 +1,30 @@
+// תרגיל 1
+
+// let input = document.getElementById("input")
+
+// input.placeholder = "user name"
+// input.style.fontSize = '20px';
+
+// תרגיל שני
+
+// let ul = document.getElementById("ul")
+
+// for(let i = 0; i < 4; i++){
+//     const li = document.createElement("li")
+//     li.innerHTML='hello'
+//     const br = document.createElement("br")
+//     ul.append(li);
+//     ul.append(br);
+// }
+
+
+// תרגיל 3
+
+// const ul = document.querySelector("#ul")
+// const list = ul.children[2]
+// list.style.color = "red";
+// list.style.fontSize = '70px';
+
+// list.innerHTML = "not..."
+
+
